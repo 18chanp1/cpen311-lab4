4d5448f08e7cb3c2a77430c008da5fe0a14cdd30
18chanp1 <89321535+18chanp1@users.noreply.github.com>
2023-06-13 15:13:10 -0700

    additional comments, awaiting testing

--- rtl/our_components/task2/decrypter.sv
+++ rtl/our_components/task2/decrypter.sv
@@ -27,39 +27,43 @@ module decrypter
     /* Signals synchronous*/
 );
 
-    localparam MSG_ADDR_T =     ADDR_WIDTH + 7 - 1; //default 14
-    localparam RESULT_ADDR_T =  MSG_ADDR_T + ADDR_WIDTH; //default 22
+    localparam MSG_ADDR_T =     ADDR_WIDTH + 7 - 1;         //default 14
+    localparam RESULT_ADDR_T =  MSG_ADDR_T + ADDR_WIDTH;    //default 22
     localparam RESULT_DATA_T =  RESULT_ADDR_T + DATA_WIDTH; //default 30
-    localparam S_ADDR_T =       RESULT_DATA_T + ADDR_WIDTH //default 38
-    localparam S_DATA_T =       S_ADDR_T + DATA_WIDTH //default 46
+    localparam S_ADDR_T =       RESULT_DATA_T + ADDR_WIDTH  //default 38
+    localparam S_DATA_T =       S_ADDR_T + DATA_WIDTH       //default 46
 
     logic [S_DATA_T:0] state;
-
+    
     assign finish =         state[0];
+    /* RAM write enables */
     assign result_wren =    state[1];
     assign s_wren =         state[2];
     /*assign state_ID =     state[6:3];*/
-    /* variable by param*/
+    /* variable length params*/
+    /* Encrypted message ROM control*/
     assign msg_addr =       state[MSG_ADDR_T:7];
+    /* Decrypted message RAM control*/
     assign result_addr =    state[RESULT_ADDR_T:MSG_ADDR_T + 1];
     assign result_data =    state[RESULT_DATA_T:RESULT_ADDR_T + 1];
+    /* Scratchpad RAM controls*/
     assign s_addr =         state[S_ADDR_T:RESULT_DATA_T + 1];
     assign s_data =         state[S_DATA_T:S_ADDR_T + 1];
 
+    /*registers to store variables*/
     logic[ADDR_WIDTH - 1:0] i, j;
     logic[DATA_WIDTH - 1:0] si, sj;
     logic [$clog2(MESSAGE_LEN) - 1 : 0] k;
 
-    logic 
-
+    /* State encodings*/
     parameter READY =           7'b0000_000;
     parameter LOAD_SI =         7'b0001_000;
-    parameter COMPUTE_J =       7'b0010_000;
+    parameter COMPUTE_J =       7'b0010_000; /* Wait for S[i] to be retrieved*/
     parameter LOAD_SJ =         7'b0011_000;
     parameter WRITE_SI_TO_SJ =  7'b0100_100;
     parameter WRITE_SJ_TO_SI =  7'b0101_100;
     parameter GET_F_ENCR =      7'b0110_000;
-    parameter COMPUTE_DECRYPT = 7'b0111_000;
+    parameter COMPUTE_DECRYPT = 7'b0111_000;/*Wait for S[s[i]+s[j]] to be retrieved*/
     parameter WRITEOUT =        7'b1000_010;
     parameter DONE =            7'b1001_001;
 
@@ -76,7 +80,7 @@ module decrypter
         else
         begin
             case (state[6:0])
-                READY: 
+                READY: //s_q = xx
                 begin
                     if(start) 
                     begin
@@ -90,7 +94,7 @@ module decrypter
                 begin
                     state[6:0]                              <= COMPUTE_J;
                 end
-                COMPUTE_J: //s_q=s[i]
+                COMPUTE_J: //s_q = s[i]
                 begin
                     j                                       <= j + s_q;
                     si                                      <= s_q;
@@ -120,7 +124,7 @@ module decrypter
                 begin
                     state[6:0]                              <= COMPUTE_DECRYPT;
                 end
-                COMPUTE_DECRYPT: //s_q = s[s[i] + s[j]]
+                COMPUTE_DECRYPT: //s_q = s[s[i] + s[j]], msg_q = msg[k]
                 begin
                     state[RESULT_DATA_T:RESULT_ADDR_T + 1]  <= s_q ^ msg_q; //result_data
                     state[RESULT_ADDR_T:MSG_ADDR_T + 1]     <= k;           //result_addr
